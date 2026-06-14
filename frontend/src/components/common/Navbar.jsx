80b28da40c148ba78cad2b8c6e18e8f0cc91763b
KaifSoomro <kaifsoomro82@gmail.com>
2026-06-15 04:05:40 +0500

    changed UI/UX for better performance

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import { SunDim } from "lucide-react";
+import { Menu, SunDim } from "lucide-react";
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="w-[95%] px-3 md:max-w-3xl lg:max-w-4xl xl:max-w-[1600px] h-12 md:h-13 lg:h-15 flex items-center justify-between border border-white/15 rounded-full mt-4 backdrop-blur-md bg-white/2 shadow-[inset_0_2px_2px_rgba(255,255,255,0.08),inset_0_-3px_5px_rgba(126,217,86,0.15),0_10px_30px_rgba(0,0,0,0.35)] fixed top-0">
+      <div className="w-[95%] px-3 md:max-w-3xl lg:max-w-4xl xl:max-w-[1600px] h-12 md:h-13 lg:h-15 flex items-center justify-between border border-white/15 rounded-full mt-4 backdrop-blur-lg bg-white/2 shadow-[inset_0_2px_2px_rgba(255,255,255,0.08),inset_0_-3px_5px_rgba(71,243,117,0.15),0_10px_30px_rgba(0,0,0,0.35)] fixed top-0">
         <Link to="/" className="ms-1">
           <img src="/logo_1.png" alt="" className="w-35 md:w-40 lg:w-43" />
         </Link>
@@ -29,17 +29,20 @@ const Navbar = () => {
           </NavLink>
         </div>
 
-        <div className="md:flex items-center gap-5 hidden">
-          <button className="scale-120 hover:scale-140 cursor-pointer hover:text-yellow-500 transition-all duration-200">
+        <div className="flex items-center gap-1 md:gap-5">
+          <button className="scale-110 hover:scale-140 cursor-pointer hover:text-yellow-500 transition-all duration-200">
             <SunDim />
           </button>
           <Link
             to="/login"
             className="bg-none px-8 py-2 rounded-full text-lg font-semibold border-2 border-[#7ed956]/20 transition-all duration-200 ease-out
-            hover:shadow-[0_0_20px_rgba(126,217,86,0.5)] text-[#7ed956]"
+            hover:shadow-[0_0_20px_rgba(126,217,86,0.5)] text-[#47f375] hidden md:block"
           >
             Login
           </Link>
+          <button className="block md:hidden">
+            <Menu className="w-9 text-[#47f375]" />
+          </button>
         </div>
       </div>
     </div>
--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -6,33 +6,27 @@ import { ArrowRight } from "lucide-react";
 const Hero = () => {
   return (
     <Container>
-      <div className="w-full md:mt-30 h-160 md:h-180 relative flex items-center justify-center">
-        <img
-          src="/public/hero.png"
-          alt=""
-          className="w-full object-contain blur-sm md:blur-lg lg:blur-xl absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] -z-1"
-        />
-
-        <div className="text-center">
+      <div className="w-full mt-27 md:mt-30 h-140 md:h-180 flex flex-col-reverse md:flex-row items-center justify-center">
+        <div className="md:text-start text-center mt-7 md:mt-0">
           <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold tracking-tight">
-            Your Digital Vault for a{" "}
-            <span className="text-[#7ed956] lg:text-[#071800]">Safer</span> Internet.
+            Your Digital Vault for a <span className="text-[#47f375]">Safer</span> Internet.
           </h1>
-          <p className="md:w-200 mt-3 text-xl md:text-2xl mx-auto bg-linear-to-l from-neutral-100 leading-8
-          via-neutral-300 to-neutral-100 bg-clip-text text-transparent">
+          <p className="md:w-200 mt-3 md:mt-7 text-md md:text-2xl text-neutral-300 leading-6 md:leading-8">
             Take control of your digital security with a modern password manager
             that keeps all your credentials encrypted, organized, and instantly
             accessible whenever you need them.
           </p>
-          <div className="mt-5 md:mt-10 flex items-center justify-center">
+          <div className="mt-5 md:mt-8 flex items-center justify-center md:justify-start">
             <button
               to="/login"
-              className="md:px-5 px-3 md:py-3 py-2 text-black rounded-full text-lg md:text-xl transition-all duration-200 ease-out bg-[#7ed956] flex items-center gap-3
-            hover:shadow-[0_0_20px_rgba(126,217,86,0.5)] hover:scale-105 font-bold group"
+              className="md:px-5 px-3 md:py-3 py-2 text-black rounded-full text-lg md:text-xl transition-all duration-200 ease-out bg-[#47f375] flex items-center gap-3 hover:shadow-[0_0_20px_rgba(126,217,86,0.5)] hover:scale-105 font-bold group"
             >
               <span>Get Started</span> <ArrowRight className="group-hover:-rotate-45 transition-all duration-200 ease-out"/>
             </button>
           </div>
+        </div>
+        <div className="mt-7 md:mt-0">
+          <img src="./public/hero_2.png" alt="" className="w-75 md:w-200"/>
         </div>
       </div>
     </Container>
