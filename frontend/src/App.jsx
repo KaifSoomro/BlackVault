1520f5a5668d3437e0c05f6aefdb244f88bea741
KaifSoomro <kaifsoomro82@gmail.com>
2026-06-15 23:31:46 +0500

    login page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Navbar from "./components/common/Navbar.jsx";
 import ManagePass from "./pages/ManagePass.jsx";
+import Login from "./pages/Login.jsx";
 
 const App = () => {
+  const location = useLocation();
+
+  const showNavbar = () => {
+    if(location.pathname === "/login" || location.pathname === "/signup"){
+      return 
+    }else {
+      return <Navbar />
+    }
+  }
   return (
     <>
-      <Navbar />
+      { showNavbar() }
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/passwords" element={<ManagePass />} />
       </Routes>
     </>
