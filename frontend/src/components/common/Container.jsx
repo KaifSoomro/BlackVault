fcdeec2ece92febf67c2c530528943f94b48e4a7
KaifSoomro <kaifsoomro82@gmail.com>
2026-06-16 03:15:32 +0500

    login page responsive

--- frontend/src/components/common/Container.jsx
+++ frontend/src/components/common/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({ children }) => {
   return (
-    <div className='w-full px-5 md:px-0 md:max-w-3xl lg:max-w-4xl xl:max-w-[1600px] mx-auto'>
+    <div className='w-full px-4 md:px-0 md:max-w-3xl lg:max-w-4xl xl:max-w-[1600px] mx-auto'>
         { children }
     </div>
   )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,25 +6,25 @@ import "../index.css";
 const Login = () => {
   return (
     <Container>
-      <div className="w-full mt-20 rounded-3xl p-px relative overflow-hidden">
+      <div className="w-full md:mt-20 mt-20 rounded-3xl p-px relative overflow-hidden">
         <div className="absolute inset-0 rounded-3xl">
           <div className="absolute -inset-full animate-spin-slow bg-[conic-gradient(from_0deg,transparent_0deg,transparent_300deg,#47f375_330deg,transparent_360deg)]" />
         </div>
 
-        <div className="relative bg-linear-to-tr from-black to-neutral-900 h-190 rounded-3xl flex items-center flex-1 p-6 border border-neutral-900">
-          <div className="w-[50%] h-full relative">
+        <div className="relative bg-linear-to-tr from-black to-neutral-900 md:h-190 rounded-3xl md:flex md:flex-row items-center flex-1 p-5 border border-neutral-900">
+          <div className="hidden w-[50%] h-full md:block relative">
             <img
               src="./public/logo_2.png"
               alt=""
-              className="absolute top-15 left-15 w-15"
+              className="absolute top-15 left-15 md:w-15 w-10"
             />
             <img
               src="./public/login_img.jpg"
               alt=""
-              className="w-full h-full rounded-3xl"
+              className="w-full h-full rounded-3xl hidden md:block"
             />
 
-            <div className="w-[70%] p-10 rounded-2xl backdrop-blur-lg absolute bottom-15 left-15 border border-white/10">
+            <div className="hidden md:block absolute w-[70%] p-10 rounded-2xl backdrop-blur-lg bottom-15 left-15 border border-white/10">
               <h1 className="font-bold text-4xl">Login to Your Vault</h1>
               <p className="text-xl mt-4">
                 Login to securely manage, store, and access your passwords
@@ -33,19 +33,21 @@ const Login = () => {
             </div>
           </div>
 
-          <div className="w-[50%] px-40">
+          <div className="md:w-[50%] md:px-40 mt-10 md:mt-0">
             <form>
-              <div className="w-full flex flex-col justify-center items-center">
-                <div className="w-full text-lg">
-                  <h1 className="text-5xl font-bold">Welcome back.</h1>
-                  <p className="mt-4 text-xl text-neutral-400">
+              <div className="w-full md:flex flex-col justify-center items-center">
+                <div className="w-full md:text-lg">
+                  <h1 className="md:text-5xl text-4xl font-bold">
+                    Welcome back.
+                  </h1>
+                  <p className="mt-2 md:mt-4 text-md md:text-xl text-neutral-400">
                     Access your encrypted vault and manage your passwords with
                     confidence.
                   </p>
                   <input
                     type="email"
                     placeholder="Email Address"
-                    className="w-full mt-8 h-12 ps-3 border border-neutral-700 rounded-lg outline-none focus:border-lime-600 bg-transparent transition-all ease-in-out duration-200"
+                    className="w-full mt-5 md:mt-8 h-12 ps-3 border border-neutral-700 rounded-lg outline-none focus:border-lime-600 bg-transparent transition-all ease-in-out duration-200"
                   />
                   <input
                     type="password"
@@ -53,17 +55,30 @@ const Login = () => {
                     className="w-full mt-6 h-12 ps-3 border border-neutral-700 rounded-lg outline-none focus:border-lime-600 bg-transparent transition-all ease-in-out duration-200"
                   />
 
-                  <button className="w-full mt-6 md:py-2 text-black rounded-lg text-lg md:text-xl transition-all duration-200 ease-out bg-[#8abc01] hover:shadow-[0_0_20px_rgba(126,217,86,0.5)] hover:scale-102 font-bold text-center">
+                  <button className="w-full mt-6 py-2 md:py-2 text-black rounded-lg text-lg md:text-xl transition-all duration-200 ease-out bg-[#8abc01] hover:shadow-[0_0_20px_rgba(126,217,86,0.5)] hover:scale-102 font-bold text-center">
                     Login
                   </button>
 
-                  <div className="text-center mt-6 text-[#8abc01] hover:underline">
-                    <Link to={"/forgot-password"}>Forgot Password?</Link>
+                  <div className="text-center mt-3 md:mt-6 text-[#8abc01] hover:underline mb-8">
+                    <Link
+                      to={"/forgot-password"}
+                      className="text-sm md:text-lg"
+                    >
+                      Forgot Password?
+                    </Link>
                   </div>
 
-                  <div className="relative mt-10">
-                    <hr className="text-neutral-800"/>
-                    <span className="bg-black text-sm px-3 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-neutral-500">Don't have an account? <Link to="/signup" className="text-[#8abc01] font-semibold">Signup</Link></span>
+                  <div className="relative md:mt-10 mt-5 text-center mb-4">
+                    <hr className="text-neutral-800" />
+                    <span className="w-60 bg-black text-xs md:text-sm md:px-3 px-1 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-neutral-500">
+                      Don't have an account?{" "}
+                      <Link
+                        to="/signup"
+                        className="text-[#8abc01] font-semibold"
+                      >
+                        Signup
+                      </Link>
+                    </span>
                   </div>
                 </div>
               </div>
