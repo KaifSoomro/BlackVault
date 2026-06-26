f82f30cf6a2b84470bde783703a4728e2ce86c12
KaifSoomro <kaifsoomro82@gmail.com>
2026-06-27 01:07:36 +0500

    Login page animation

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,7 +6,8 @@ import { ShieldCheck } from "lucide-react";
 import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
-import { setUser } from "../features/userSlice.js"
+import { setUser } from "../features/userSlice.js";
+import { motion } from "framer-motion";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -74,17 +75,25 @@ const Login = () => {
 
   return (
     <Container>
-      <div className="w-full md:mt-20 mt-20 rounded-3xl p-px relative overflow-hidden">
+      <motion.div
+        initial={{ y: 30, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4 }}
+        className="w-full md:mt-20 mt-20 rounded-3xl p-px relative overflow-hidden"
+      >
         <div className="absolute inset-0 rounded-3xl">
           <div className="absolute -inset-full animate-spin-slow bg-[conic-gradient(from_0deg,transparent_0deg,transparent_300deg,#47f375_330deg,transparent_360deg)]" />
         </div>
 
         <div className="relative bg-linear-to-tr from-black to-neutral-900 md:h-190 rounded-3xl md:flex md:flex-row items-center flex-1 p-5 border border-neutral-900">
           <div className="hidden w-[50%] h-full md:block relative">
-            <img
+            <motion.img
               src="/logo_2.png"
               alt=""
               className="absolute top-8 left-9 md:w-15 w-10"
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.4, delay: 0.2 }}
             />
             <img
               src="/login_img.jpg"
@@ -92,31 +101,51 @@ const Login = () => {
               className="w-full h-full rounded-3xl hidden md:block"
             />
 
-            <div className="absolute top-8 right-8 flex items-center gap-2 px-4 py-2 rounded-full bg-black/30 backdrop-blur-xl border border-white/10">
+            <motion.div
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.4, delay: 0.1 }}
+              className="absolute top-8 right-8 flex items-center gap-2 px-4 py-2 rounded-full bg-black/30 backdrop-blur-xl border border-white/10"
+            >
               <ShieldCheck size={16} className="text-[#47f375]" />
               <span className="text-sm">End-to-End Encrypted</span>
-            </div>
+            </motion.div>
 
-            <div className="hidden md:block absolute w-[70%] p-10 rounded-2xl backdrop-blur-lg bottom-8 left-8 border border-white/10">
+            <motion.div
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.4, delay: 0.3 }}
+              className="hidden md:block absolute w-[70%] p-10 rounded-2xl backdrop-blur-lg bottom-8 left-8 border border-white/10"
+            >
               <h1 className="font-bold text-4xl">Login to Your Vault</h1>
               <p className="text-xl mt-4">
                 Login to securely manage, store, and access your passwords
                 anytime.
               </p>
-            </div>
+            </motion.div>
           </div>
 
           <div className="md:w-[50%] md:px-40 mt-10 md:mt-0">
             <form onSubmit={handleSubmit}>
               <div className="w-full md:flex flex-col justify-center items-center">
                 <div className="w-full md:text-lg">
-                  <h1 className="md:text-5xl text-4xl font-bold">
+                  <motion.h1
+                    initial={{ y: 20, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.4, delay: 0.5 }}
+                    className="md:text-5xl text-4xl font-bold"
+                  >
                     Welcome back.
-                  </h1>
-                  <p className="mt-2 md:mt-4 text-md md:text-xl text-neutral-400">
+                  </motion.h1>
+                  <motion.p
+                    initial={{ y: 20, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.4, delay: 0.6 }}
+                    className="mt-2 md:mt-4 text-md md:text-xl text-neutral-400"
+                  >
                     Access your encrypted vault and manage your passwords with
                     confidence.
-                  </p>
+                  </motion.p>
                   <input
                     type="email"
                     placeholder="Email Address"
@@ -134,9 +163,15 @@ const Login = () => {
                     className="w-full mt-6 h-12 ps-3 border border-neutral-700 rounded-lg outline-none focus:border-lime-600 bg-transparent transition-all ease-in-out duration-200"
                   />
 
-                  <button type="submit" className="w-full mt-6 py-2 md:py-2 text-black rounded-lg text-lg md:text-xl transition-all duration-200 ease-out bg-[#8abc01] hover:shadow-[0_0_20px_rgba(126,217,86,0.5)] hover:scale-102 font-bold text-center">
-                    { isPending ? "Unlocking Vault..." : "Login"  }
-                  </button>
+                  <motion.button
+                    initial={{ scale: 0.8, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{ duration: 0.4, delay: 0.7 }}
+                    type="submit"
+                    className="w-full mt-6 py-2 md:py-2 text-black rounded-lg text-lg md:text-xl transition-all duration-200 ease-out bg-[#8abc01] hover:shadow-[0_0_20px_rgba(126,217,86,0.5)] hover:scale-102 font-bold text-center"
+                  >
+                    {isPending ? "Unlocking Vault..." : "Login"}
+                  </motion.button>
 
                   <div className="text-center mt-3 md:mt-6 text-[#8abc01] hover:underline mb-8">
                     <Link
@@ -164,7 +199,7 @@ const Login = () => {
             </form>
           </div>
         </div>
-      </div>
+      </motion.div>
     </Container>
   );
 };
