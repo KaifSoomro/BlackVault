fc4bc33bed2e194c9a68731d9dede060a989dfea
KaifSoomro <kaifsoomro82@gmail.com>
2026-07-18 15:38:58 +0500

    get request for server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/accounts", accountRouter);
 
+app.get("/", (req, res) => {
+    res.send({
+        success: true,
+        message: "Server is running"
+    })
+})
+
 await connectDB();
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
